d39e28900e7734e65ac04846eb1e3afc0c1d0135
SidaliAdoul <adoul.sidou@gmail.com>
2020-10-18 18:45:41 +0100

    zdt log in logout signup ga3

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import BlogListView,BlogDetailView
+from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
 from .models import Post
 
 urlpatterns = [
     path("", BlogListView.as_view(model=Post), name="home"),
     path("post/<int:pk>/", BlogDetailView.as_view(model=Post), name="post_detail"),
+    path("post/new/", BlogCreateView.as_view(model=Post), name="post_new"),
+    path("post/<int:pk>/edit", BlogUpdateView.as_view(model=Post), name="post_edit"),
+    path("post/<int:pk>/delete", BlogDeleteView.as_view(model=Post), name="post_delete")
 ]
